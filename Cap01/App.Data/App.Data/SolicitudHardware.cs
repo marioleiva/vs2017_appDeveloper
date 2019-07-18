060a83bd5907e6f5f72a9e8fd344f7baadf6f835
marioLeiva <mario.leiva24@gmail.com>
2019-07-17 22:14:13 -0500

    UnitTest

---
+++ Cap01/App.Data/App.Data/SolicitudHardware.cs
@@ -0,0 +1,14 @@
+﻿using App.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data //.Base  // Agrupador ejem system.IO    System.Data
+{
+    public class SolicitudHardware : Solicitud
+    {
+
+    }
+}
---
+++ Cap01/App.Data/App.DataUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App.DataUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App.DataUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d831d0f-d639-4824-a645-9225f990f718")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
