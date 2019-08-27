f0d752b6f5fd0875b6795bfc1ec92510437ce78a
marioleiva <mario.leiva24@gmail.com>
2019-08-27 16:52:12 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/marioleiva/vs2017_appDeveloper""
    This reverts commit b0b49fbc80ab309f3418df173921755386b4f5fc.

---
+++ ShopBaseArchitectureExample/Shop.Presentation.WebPage/Controllers/ShedulerTaskController.cs
@@ -0,0 +1,32 @@
+﻿using Shop.Presentation.WebPage.Filtros;
+using Shop.Services.implementation;
+using Shop.Services.Interfaces.Handlers;
+using Shop.Services.Interfaces.Requests;
+using Shop.Services.Interfaces.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Shop.Presentation.WebPage.Controllers
+{
+    [FiltroDeExcepciones]
+    public class ShedulerTaskController : ApiController
+    {
+        ISheduleTaskHandler _sheduleTaskHandler = new EFShedulerTaskHandler();
+
+        [HttpGet]
+        public IEnumerable<RegisteredShedulerTask> GetAllShedulerTask()
+        {
+            return this._sheduleTaskHandler.GetAllSheduler();
+        }
+
+        [HttpPost]
+        public RegisteredShedulerTask RegisterSheduler(CreateShedulerTask newSheduler)
+        {
+            return this._sheduleTaskHandler.RegisterSheduler(newSheduler);
+        }
+    }
+}
---
+++ ShopBaseArchitectureExample/Shop.Services.Interfaces/Requests/CreateShedulerTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Services.Interfaces.Requests
+{
+    public class CreateShedulerTask
+    {
+        public string Description { get; set; }
+        public bool Complete { get; set; }
+        public DateTime SheduledFor { get; set; }
+    }
+}
