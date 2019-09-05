9d71e99c221b7aff29d82da5a07dad411ff51910
Mario Leiva <mario.leiva24@gmail.com>
2019-09-04 22:22:31 -0500

    Servicio Windows

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Quartz;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(windowsService =>
+            {
+                windowsService.ScheduleQuartzJobAsService(service => {
+                    service.WithJob(() => JobBuilder.Create<Sincronizar>().Build())
+                           .AddTrigger(() => TriggerBuilder.Create()
+                                            .WithSimpleSchedule(action =>
+                                                action.WithIntervalInSeconds(5).RepeatForever())
+                                            .Build());
+                });
+            });
+        }
+
+    }
+
+    public class Sincronizar : IJob
+    {
+
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("algo");
+        }
+    }
+}
