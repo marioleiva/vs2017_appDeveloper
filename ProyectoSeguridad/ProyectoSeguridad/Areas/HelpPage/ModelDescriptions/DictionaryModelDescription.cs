768596e8913a70075df8449a092f13d205d7e47d
Mario Leiva <mario.leiva24@gmail.com>
2019-09-09 22:31:07 -0500

    20190909

---
+++ ProyectoSeguridad/ProyectoSeguridad/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProyectoSeguridad.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ProyectoSeguridad/ProyectoSeguridad/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoSeguridad.WebApiApplication" Language="C#" %>
