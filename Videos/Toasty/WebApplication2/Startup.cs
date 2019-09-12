559d80af3d87e40579b313c03b757b977c5ef7af
Mario Leiva <mario.leiva24@gmail.com>
2019-09-11 22:25:29 -0500

    clase 201910911

---
+++ Videos/Toasty/WebApplication2/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication2.Startup))]
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/signalr", map =>
+            {
+                var hubConfiguration = new HubConfiguration();
+                hubConfiguration.EnableDetailedErrors = true;
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+    }
+}
